44652e8d199eb4ab74b5fc2f8718e3a4828c9c0d
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-05 09:04:05 -0300

    estrutura inicial

---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    gray: {
+        100: "#f2f2f2",
+        200: "#d9d9d9",
+        300: "#808080",
+        400: "#333333",
+        500: "#262626",
+        600: "#1a1a1a",
+        700: "#0d0d0d",
+    },
+    purple: {
+        default: "#8284fa",
+        dark: "#5e60ce",
+    },
+    blue: {
+        default: "#4ea8d3",
+        dark: "#1e6f9f",
+    },
+    danger: "#e25858",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import { colors } from "./src/styles/colors"
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors
+    },
+  },
+  plugins: [],
+}
